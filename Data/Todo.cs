bca23871cc89aaca2d915e6f3ba6a2fca6fa8999
Joe Zhou <jzhou2@ctscorp.com>
2023-06-13 16:46:09 +0800

    Init QFamilyForum

---
+++ Data/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace QFamilyForum.Data
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get;set;}
+        public DateTime EndTime { get;set;}
+    }
+}
---
+++ Data/TodoService.cs
@@ -0,0 +1,33 @@
+﻿namespace QFamilyForum.Data
+{
+    public class TodoService
+    {
+        private List<TodoItem> todos = new List<TodoItem>{};
+        public TodoService() 
+        {
+            todos = Enumerable.Range(1, 5).Select(index => new TodoItem
+            {
+                Id = index,
+                Title = $"Todo {index}",
+                Description = $"This is a description for Todo {index}",
+                EndTime = DateTime.Now.AddDays(index)
+            }).ToList();
+        }
+
+        public Task<TodoItem[]> GetTodoAsync()
+        {
+            
+            return Task.FromResult(todos.OrderBy(item => item.EndTime).ToArray());
+        }
+        public void AddTodo(string title)
+        {
+            Console.WriteLine($"Added Todo: {title}");
+            todos.Add(new TodoItem{
+                Id = todos.Count + 1,
+                Title = title,
+                Description = $"This is a description for Todo {todos.Count + 1}",
+                EndTime = DateTime.Now.AddDays(todos.Count + 1)
+            }); 
+        }
+    }
+}
